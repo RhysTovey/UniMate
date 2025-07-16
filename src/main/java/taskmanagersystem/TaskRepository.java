e0e78f59688dde46fb019c898e09814b791e8401
rhystovey <rhystovey019@gmail.com>
2025-07-16 16:33:18 +0100

    Added file saving/loading for task

--- src/main/java/taskmanagersystem/TaskRepository.java
+++ src/main/java/taskmanagersystem/TaskRepository.java
@@ -1,5 +1,26 @@
 package taskmanagersystem;
 
-public class TaskRepository {
+import java.io.*;
+import java.util.List;
 
+public class TaskRepository {
+    public static <T> void save(String filePath, List<T> list) {
+        try(ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filePath))) {
+            out.writeObject(list);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> List<T> load(String filePath) {
+        try(ObjectInputStream in = new ObjectInputStream(new FileInputStream(filePath))) {
+            return (List<T>) in.readObject();
+        }catch (EOFException e) {
+            return new java.util.ArrayList<>();
+        }
+        catch (IOException | ClassNotFoundException e) {
+            return new java.util.ArrayList<>();
+        }
+    }
 }
